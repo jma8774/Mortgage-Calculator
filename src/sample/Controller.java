0da73e6f574ff0a3291baeac4021f286af6a81bb
Jia Ming <jma8774@bths.edu>
2019-04-08 00:35:32 -0400

    upload

---
+++ src/sample/Controller.java
@@ -0,0 +1,162 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TextField;
+import java.util.ArrayList;
+
+public class Controller {
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private TextField purchasePriceText;
+    @FXML
+    private TextField downPaymentText;
+    @FXML
+    private TextField interestRateText;
+    @FXML
+    private TextField titleLabel;
+    @FXML
+    private TextField purchasePriceLabel;
+    @FXML
+    private TextField downPaymentLabel;
+    @FXML
+    private TextField interestRateLabel;
+    @FXML
+    private TextField yearLabel;
+    @FXML
+    private Slider slider;
+    @FXML
+    private TextField displayLabel1;
+    @FXML
+    private TextField displayLabel2;
+    @FXML
+    private TextField displayLabel3;
+    @FXML
+    private TextField displayLabel4;
+    @FXML
+    private TextField displayLabel5;
+    @FXML
+    private TextField displayLabel6;
+    @FXML
+    private TextField displayAnswer1;
+    @FXML
+    private TextField displayAnswer2;
+    @FXML
+    private TextField displayAnswer3;
+    @FXML
+    private TextField displayAnswer4;
+    @FXML
+    private TextField displayAnswer5;
+    @FXML
+    private TextField displayAnswer6;
+
+    private int purchasePrice;
+    private int downPayment;
+    private double interestRate;
+    private double interestRateMonth;
+    private int loanAmount;
+    private int year;
+    private int month;
+
+    private ArrayList<TextField> displayLabels;
+    private ArrayList<TextField> displayAnswers;
+
+    @FXML
+    public void initialize() {
+        displayLabels = new ArrayList<TextField>();
+        displayLabels.add(displayLabel1);
+        displayLabels.add(displayLabel2);
+        displayLabels.add(displayLabel3);
+        displayLabels.add(displayLabel4);
+        displayLabels.add(displayLabel5);
+        displayLabels.add(displayLabel6);
+
+        displayAnswers = new ArrayList<TextField>();
+        displayAnswers.add(displayAnswer1);
+        displayAnswers.add(displayAnswer2);
+        displayAnswers.add(displayAnswer3);
+        displayAnswers.add(displayAnswer4);
+        displayAnswers.add(displayAnswer5);
+        displayAnswers.add(displayAnswer6);
+
+
+        year = (int) slider.getValue();
+        yearLabel.setText("Select year: " + year);
+        month = year * 12;
+        displayLabel4.setText("Monthly payment in " + year + " years: ");
+
+        changeDisplayAnswers();
+    }
+
+    public void changeDisplayAnswers(){
+        loanAmount = purchasePrice - downPayment;
+        displayAnswer1.setText(Integer.toString(loanAmount));
+        displayAnswer2.setText(Double.toString(loanAmount / ((Math.pow((1+interestRateMonth),10*12)-1) / (interestRateMonth*Math.pow(1+interestRateMonth, 10*12)))));
+        displayAnswer5.setText(Double.toString(loanAmount / ((Math.pow((1+interestRateMonth),20*12)-1) / (interestRateMonth*Math.pow(1+interestRateMonth, 20*12)))));
+        displayAnswer3.setText(Double.toString(loanAmount / ((Math.pow((1+interestRateMonth),30*12)-1) / (interestRateMonth*Math.pow(1+interestRateMonth, 30*12)))));
+        displayAnswer6.setText(Double.toString(loanAmount / ((Math.pow((1+interestRateMonth),40*12)-1) / (interestRateMonth*Math.pow(1+interestRateMonth, 40*12)))));
+        displayAnswer4.setText(Double.toString(loanAmount / ((Math.pow((1+interestRateMonth),month)-1) / (interestRateMonth*Math.pow(1+interestRateMonth, month)))));
+    }
+
+    @FXML
+    public void onKeyTypePP(){
+        try {
+            purchasePrice = Integer.parseInt(purchasePriceText.getText());
+            changeDisplayAnswers();
+        } catch (Exception NumberFormatException) {
+            System.out.println("NumberFormatException: User entered a letter");
+            for(TextField t: displayAnswers)
+                t.setText("");
+        }
+    }
+
+    @FXML
+    public void onKeyTypeDP(){
+        try {
+            downPayment = Integer.parseInt(downPaymentText.getText());
+            changeDisplayAnswers();
+        } catch (Exception NumberFormatException) {
+            System.out.println("NumberFormatException: User entered a letter");
+            for(TextField t: displayAnswers)
+                t.setText("");
+        }
+    }
+
+    @FXML
+    public void onKeyTypeIR(){
+        try {
+            interestRate = Double.parseDouble(interestRateText.getText());
+            interestRateMonth = interestRate / 12;
+            changeDisplayAnswers();
+        } catch (Exception NumberFormatException) {
+            System.out.println("NumberFormatException: User entered a letter");
+            for(TextField t: displayAnswers)
+                t.setText("");
+        }
+    }
+
+    @FXML
+    public void onSliderDrag() {
+        year = (int) slider.getValue();
+        month = year * 12;
+        yearLabel.setText("Select year: " + year);
+        displayLabel4.setText("Monthly payment in " + year + " years:");
+        displayAnswer4.setText(Double.toString(loanAmount / ((Math.pow((1+interestRateMonth),month)-1) / (interestRateMonth*Math.pow(1+interestRateMonth, month)))));
+    }
+
+    @FXML
+    public void deselect(){
+        titleLabel.deselect();
+        purchasePriceLabel.deselect();
+        downPaymentLabel.deselect();
+        interestRateLabel.deselect();
+        yearLabel.deselect();
+        for(TextField t:displayLabels)
+            t.deselect();
+        for(TextField t:displayAnswers)
+            t.deselect();
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.File;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mortgage calculator.fxml"));
+        primaryStage.setTitle("Mortgage Calculator");
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("calculator.png")));
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
